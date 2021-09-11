f5b13c685e72622cfa6d72e207a1ab501c84043e
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-11 11:34:26 -0400

    Added:creatingBoard

--- src/com/bridgelabs/TicTacToe.java
+++ src/com/bridgelabs/TicTacToe.java
@@ -1,9 +1,14 @@
 package com.bridgelabs;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     static  char[] board = new char[10];
-
+    private static char cross ='X';
+    private static char zero ='O';
+    private static char player;
+    private static char computer;
     public static char[]  creatingBoard() {
 
 
@@ -11,12 +16,43 @@ public class TicTacToe {
             board[i] = ' ';
         }
         return board;
+    }
+    private static void allowPlayerToChoose()
+    {
+        System.out.println("Enter X or O you want to choose");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        player = input.charAt(0);
+
+        if(player == cross)
+        {
+            player = cross;
+            computer = zero;
+            System.out.println("player symbol : " + player);
+
+        }
+        else if(player == zero)
+        {
+            player = zero;
+            computer = cross;
+            System.out.println("player symbol : " + player);
+
+        }
+        else
+        {
+            System.out.println("Invalid Character");
+        }
+
+
 
     }
+
 
     public static void main(String args[]) {
 
         System.out.println("Welcome to TicTacToe Board Game");
         board = creatingBoard();
+
+        allowPlayerToChoose();
     }
 }
