7eb22f019edfebba15231169e46c3507ea2aee98
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-11 11:28:07 -0400

    Added:emptyBoard

--- src/com/bridgelabs/TicTacToe.java
+++ src/com/bridgelabs/TicTacToe.java
@@ -2,7 +2,21 @@ package com.bridgelabs;
 
 public class TicTacToe {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe Game");
+    static  char[] board = new char[10];
+
+    public static char[]  creatingBoard() {
+
+
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to TicTacToe Board Game");
+        board = creatingBoard();
     }
 }
